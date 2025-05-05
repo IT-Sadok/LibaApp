3fa86192d3434ff1707f28cd2bb9a4c7c5bed1d1
Je4ik <zhenya.korenovskiy@gmail.com>
2025-05-05 19:29:24 +0200

    add new branch with empty project

---
+++ HospitalDocs/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace HospitalDocs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
